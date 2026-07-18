34c7ceae421d8fe75dcf9e0215cb5bf4e14f9acd
C. Spencer Beggs <spencer@beggs.codes>
2026-07-17 22:51:01 -0400

    chore: update deps
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- package/savvy.build.ts
+++ package/savvy.build.ts
@@ -1,9 +1,3 @@
-import { defineBuild, runBuild } from "@savvy-web/bundler";
+import { build } from "@savvy-web/bundler";
 
-const config = defineBuild();
-
-export default config;
-
-if (import.meta.main) {
-	await runBuild(config, { cwd: import.meta.dirname, argv: process.argv.slice(2) });
-}
+await build();
